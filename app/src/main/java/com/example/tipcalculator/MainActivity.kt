988e47ef8b2268660048e8a2593a54d139e8c7a7
ssouzageo <ssouzageovaneo@gmail.com>
2025-03-17 19:03:08 -0300

    Strings de idiomas adicionadas.

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.tipcalculator
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,67 +21,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var percentage: Int = 0
-
-        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                percentage = 10
-            }
-
-            binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    percentage = 15
-                }
-
-                binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
-                    if (isChecked) {
-                        percentage = 20
-                    }
-                }
-            }
-        }
-        val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.num_people,
-            android.R.layout.simple_spinner_item
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerNumberOfPeople.adapter = adapter
-
-        var NumOfPeopleSelected = 0
-        binding.spinnerNumberOfPeople.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                NumOfPeopleSelected = position
-            }
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-        }
-        binding.btnClean.setOnClickListener {
-            clean()
-        }
         binding.btnDone.setOnClickListener {
+            val totalTableTemp = binding.tieTotal.text
+            val numPeopleTemp = binding.tieNumpeople.text
+            val percentageTemp = binding.tiePercentage.text
 
-            val totalTableTemp = binding.tieTotal.text 
-
-            if (totalTableTemp?.isEmpty() == true) {
+            if (totalTableTemp?.isEmpty() == true ||
+                numPeopleTemp?.isEmpty() == true ||
+                percentageTemp?.isEmpty() == true
+            ) {
                 Snackbar.make(binding.tilTotal, "Preencha todos os campos.", Snackbar.LENGTH_LONG)
                     .show()
             } else {
 
                 val totalTable: Float = totalTableTemp.toString().toFloat()
-                val numPeople: Int = NumOfPeopleSelected
+                val numPeople: Int = numPeopleTemp.toString().toInt()
+                val percentage: Int = percentageTemp.toString().toInt()
 
                 val totalTemp = totalTable / numPeople
                 val tips = totalTemp * percentage / 100
                 val totalWithTips = totalTemp + tips
 
-                val intent = Intent(this, SummaryActivity :: class.java)
+                val intent = Intent(this, SummaryActivity::class.java)
                 intent.apply {
                     putExtra("totalTable", totalTable)
                     putExtra("numPeople", numPeople)
@@ -96,10 +54,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun clean() {
-            binding.tieTotal.setText("")
-            binding.rbOptionOne.isChecked = false
-            binding.rbOptionTwo.isChecked = false
-            binding.rbOptionThree.isChecked = false
+        binding.tieTotal.setText("")
+        binding.tieNumpeople.setText("")
+        binding.tiePercentage.setText("")
     }
 }
